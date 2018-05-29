bcedf2ff32ee91ecdc5dc9fe00703b4acffd0659
Lokman Boukhoulda <boukhoulda.lokman@gmail.com>
2018-05-29 17:33:02 -0400

    Changed the script to a simplified one

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+"strict use";
+
 var color1Elem = document.getElementById("color1");
 var color2Elem = document.getElementById("color2");
 var body = document.getElementById("gradient");
 
-color1.addEventListener("input", function() {
+
+function setColor(color) {
+color.addEventListener("input", function() {
 body.style.background = "linear-gradient(to right,"+ color1Elem.value+","+ color2Elem.value;
 });
-color2.addEventListener("input", function() {
-body.style.background = "linear-gradient(to right,"+ color1Elem.value+","+ color2Elem.value;
-});
+}
+
+setColor(color1);
+setColor(color2);
