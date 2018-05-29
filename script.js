6e4bb9d717f9f32036c4abccfc27347ccd1fb4e8
Lokman Boukhoulda <boukhoulda.lokman@gmail.com>
2018-05-29 18:11:32 -0400

    Added css text

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "strict use";
-
+var css = document.querySelector("h3");
 var color1Elem = document.getElementById("color1");
 var color2Elem = document.getElementById("color2");
 var body = document.getElementById("gradient");
@@ -7,6 +7,7 @@ var body = document.getElementById("gradient");
 
 function setColor() {
 body.style.background = "linear-gradient(to right,"+ color1Elem.value+","+ color2Elem.value+")";
+css.textContent = body.style.background+";";
 }
 
 color1.addEventListener("input", setColor);
